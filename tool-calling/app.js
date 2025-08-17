3d06373b79d201527946a042bf3c2a153ac7a08d
akghosh111 <ghoshkiran2017@gmail.com>
2025-08-17 20:19:16 +0530

    maintaining history while passing to llm

--- tool-calling/app.js
+++ tool-calling/app.js
@@ -6,10 +6,8 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 
 async function main() {
-    const completion = await groq.chat.completions.create({
-        model:'llama-3.3-70b-versatile',
-        temperature: 0,
-        messages: [
+
+    const messages =  [
             {
                 role: 'system',
                 content:`You are a smart personal assistant who answers the asked questions.
@@ -21,7 +19,68 @@ async function main() {
                 content: `When was iPhone 16 launched?`,
 
             }
+        ]
+    const completion = await groq.chat.completions.create({
+        model:'llama-3.3-70b-versatile',
+        temperature: 0,
+        messages: messages,
+        tools:[
+            {
+                "type": "function",
+                "function": {
+                    "name": "webSearch",
+                    "description": "Search the latest information and realtime data on the Internet.",
+                    "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "query": {
+                            "type": "string",
+                            "description": "The search query to perform search on."
+                        },
+                        
+                    },
+                    "required": ["query"]
+                    }
+                }
+            }
         ],
+        tool_choice: 'auto',
+    });
+
+    //pushing the assistant to messages so that it remains in the history
+    messages.push(completion.choices[0].message);
+
+
+    const toolCalls = completion.choices[0].message.tool_calls
+
+    if(!toolCalls) {
+        console.log(`Assistant: ${completion.choices[0].message.content}`);
+        return
+    }
+
+    //if there are more tools
+    for (const tool of toolCalls) {
+        console.log('tool:', tool)
+        const functionName = tool.function.name;
+        const functionParams = tool.function.arguments;
+
+        if(functionName === 'webSearch') {
+            const toolResult = await webSearch(JSON.parse(functionParams))
+            console.log("Tool result:", toolResult);
+
+            messages.push({
+                tool_call_id:tool.id,
+                role: 'tool',
+                name: functionName,
+                content: toolResult,
+            })
+        }
+    }
+
+    const completion2 = await groq.chat.completions.create({
+        model:'llama-3.3-70b-versatile',
+        temperature: 0,
+        messages: messages,
         tools:[
             {
                 "type": "function",
@@ -46,28 +105,9 @@ async function main() {
     });
 
 
-    const toolCalls = completion.choices[0].message.tool_calls
-
-    if(!toolCalls) {
-        console.log(`Assistant: ${completion.choices[0].message.content}`);
-        return
-    }
-
-    //if there are more tools
-    for (const tool of toolCalls) {
-        console.log('tool:', tool)
-        const functionName = tool.function.name;
-        const functionParams = tool.function.arguments;
-
-        if(functionName === 'webSearch') {
-            const toolResult = await webSearch(JSON.parse(functionParams))
-            console.log("Tool result:", toolResult);
-        }
-    }
 
 
-
-    // console.log(JSON.stringify(completion.choices[0].message, null, 2));
+    console.log(JSON.stringify(completion2.choices[0].message, null, 2));
 }
 
 main();
