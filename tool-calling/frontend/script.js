bc620c323748a6738567c77b35a2b4881abef463
akghosh111 <ghoshkiran2017@gmail.com>
2025-08-18 20:39:30 +0530

    making chatui functional

--- tool-calling/frontend/script.js
+++ tool-calling/frontend/script.js
@@ -1,1 +1,32 @@
-console.log("Working!!!")
+const input = document.querySelector('#input');
+const chatContainer = document.querySelector('#chat-container');
+
+
+
+input?.addEventListener('keyup', handleEnter);
+
+function generate(text) {
+//     append message to UI 
+//     send it to the LLM 
+//     append response to the UI
+    const msg = document.createElement('div')
+    msg.className = `my-6 bg-neutral-800 p-3 rounded-xl ml-auto max-w-fit`
+    msg.textContent = text
+    chatContainer.appendChild(msg);
+    input.value = '';
+ }
+
+
+function handleEnter(e) {
+    if(e.key === 'Enter') {
+        const text = input?.value.trim();
+        if(!text) {
+            return;
+        }
+
+
+        generate(text)
+
+        
+    }
+}
