8137253d0924fe2ee072d883a21d228e01d85ef1
akghosh111 <ghoshkiran2017@gmail.com>
2025-08-21 15:34:22 +0530

    RAG implementation plan

--- company-chatbot/index.js
+++ company-chatbot/index.js
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+/**
+ * Implementation Plan
+ * Stage 1: Indexing
+ * 1. Load the document - pdf, text
+ * 2. Chunk the document
+ * 3. Generate vector embeddings
+ * 4. Store the vector embeddings - Vector database
+ * 
+ * Stage 2: Using the chatbot
+ * 1. Setup LLM
+ * 2. Add retrieval step
+ * 3. Pass User input + relevant info --> LLM
+ * 4. LLM gives you the output
+ */
