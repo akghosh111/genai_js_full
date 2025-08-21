0ad599852711159ae5f9c93582ed690ccb1fb952
akghosh111 <ghoshkiran2017@gmail.com>
2025-08-21 16:19:13 +0530

    pure text content as response

--- company-chatbot/prepare.js
+++ company-chatbot/prepare.js
@@ -3,8 +3,8 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
 export async function indexTheDocument(filePath) {
     const loader = new PDFLoader(filePath, { splitPages: false });
-    const docs = await loader.load()
+    const doc = await loader.load()
 
 
-    console.log(docs);
+    console.log(doc[0].pageContent);
 }
