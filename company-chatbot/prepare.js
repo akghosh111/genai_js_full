fc5b151dffc29706ad07026ea3da188cd174dee1
akghosh111 <ghoshkiran2017@gmail.com>
2025-08-21 16:18:14 +0530

    load as a single doc

--- company-chatbot/prepare.js
+++ company-chatbot/prepare.js
@@ -2,9 +2,9 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
 
 export async function indexTheDocument(filePath) {
-    const loader = new PDFLoader(filePath)
+    const loader = new PDFLoader(filePath, { splitPages: false });
     const docs = await loader.load()
 
 
-    console.log(docs.length);
+    console.log(docs);
 }
